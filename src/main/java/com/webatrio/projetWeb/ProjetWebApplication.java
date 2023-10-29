e53a013b619b9e1d278c2857bd8280e5e9f840c5
kedidi1 <kedidi1995@gmail.com>
2023-10-29 16:01:29 +0100

    First commit
    Creation d'un nouveau projet

---
+++ src/main/java/com/webatrio/projetWeb/ProjetWebApplication.java
@@ -0,0 +1,13 @@
+package com.webatrio.projetWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webatrio/projetWeb/ProjetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webatrio.projetWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
